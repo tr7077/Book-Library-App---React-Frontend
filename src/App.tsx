af510f061e23ba832abd3ffecefcbd5b4b87e23b
teorerras <teorerras@gmail.com>
2025-09-06 23:35:29 +0300

    added search page with category filter and search by title + pagination

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import { Navbar } from './layout/NavbarAndFooter/Navbar';
 import { Footer } from './layout/NavbarAndFooter/Footer';
 import { HomePage } from './layout/HomePage/HomePage';
+import { SearchBooksPage } from './layout/SearchBooksPage/SearchBooksPage';
 
 export const App = () => {
   return (
     <div>
       <Navbar />
-      <HomePage />
+      {/* <HomePage /> */}
+      <SearchBooksPage />
       <Footer />
     </div>
   );
